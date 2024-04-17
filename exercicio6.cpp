3c4389c0934eb0ed5286a94f16c59721cee7ead1
luisfelipe207 <162385788+luisfelipe207@users.noreply.github.com>
2024-04-17 00:36:55 +0000

    exercicios array

--- exercicio6.cpp
+++ exercicio6.cpp
@@ -5,7 +5,6 @@ int main() {
    int numeros[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     cout << numeros[0] << endl;
-    if (numeros % 2 == 0);
     cout << numeros[1] << endl;
     cout << numeros[2] << endl;
     cout << numeros[3] << endl;
