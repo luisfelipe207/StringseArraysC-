a7522174fd2a3f948d12356e8c645df61459354b
luisfelipe207 <162385788+luisfelipe207@users.noreply.github.com>
2024-04-17 20:53:51 +0000

    exercicios array

--- exercicio6.cpp
+++ exercicio6.cpp
@@ -2,21 +2,13 @@
 using namespace std;
 
 int main() {
-   int numeros[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+   int numeros[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9,};
 
-    cout << numeros[0] << endl;
     cout << numeros[1] << endl;
-    cout << numeros[2] << endl;
     cout << numeros[3] << endl;
-    cout << numeros[4] << endl;
     cout << numeros[5] << endl;
-    cout << numeros[6] << endl;
     cout << numeros[7] << endl;
-    cout << numeros[8] << endl;
     cout << numeros[9] << endl;
-
-
-
 
 return 0;
 }
---
+++ exercicio7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+char frutas[5] = {"Banana", 'Maçã', 'Morango', 'Abacaxi', 'Laranja'};
+
+cout << "A fruta que está na posição 3 é: " << frutas[3] << endl;
+
+return 0;
+}
+
+
